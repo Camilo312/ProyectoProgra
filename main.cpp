16ef1cfd188181c6589d6bea3f5e6ffe1b6ff331
Camilo312 <camilo.munoz03@uptc.edu.co>
2021-08-13 17:20:36 -0500

    Agregar metodo caracteres

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello hola  !" << std::endl;
     return 0;
+}
+
+string deleteCharacter(char c, string chain){
+    char temp = NULL;
+    for (char & i : chain) {
+        if(i == c){
+            i = temp;
+            cout<<chain;
+        }
+    }
+    return chain;
 }
