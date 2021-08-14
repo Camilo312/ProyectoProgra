9be858fcd6a8c75c2ba0d861dc4443bc12d60d00
jazmin-saleh <jazmin.saleh@uptc.edu.co>
2021-08-14 16:21:01 -0500

    Metodo contar la palabra, encriptar y desencripatar

--- main.cpp
+++ main.cpp
@@ -1,12 +1,104 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
 
 using namespace std;
+
+int WordNumber(char sentence[ ],char word[ ]);
+string encrypt(char sentence[ ]);
+string desencripatr(char sentence[]);
 int main() {
+    int menu;
+    char sentence[ ]="Sogamoso Ciudad del Sol y del Acero";
+    char word[ ]="del";
    cout<<"1.Convertir en nombre propio el contenido de la cadena\n2.Buscar palabra\n3.Encriptar cadena\n4.Desencriptar"
          "\n5.Llenar cacarter por izquierda o por derecha\n6.Borrar cracteres de una cadena\n7.Interseccion\n8.Diferencia entre dos cadenas"
          "\n9.Borrar caracteres iziquierda o derecha\n"
-         "10.Validar correo electronico\n11.Salir";
+         "10.Validar correo electronico\n11.Salir"<<endl;
+   cin>>menu;
+
+    switch (menu) {
+        case 1:
+            break;
+        case 2:
+            int count;
+            char *tok;
+            tok = strtok(sentence," .,:;");
+            count=0;
+            while(tok != NULL){
+                if(stricmp(tok,word)==0){
+                    count++;
+                }
+                tok = strtok(NULL," .,:;");
+            }
+            cout<<"La palabra "<<word<<" se necuentra "<<count<<" veces"<<endl;
+            //No funciona la pasada por parametros
+            cout<<WordNumber(sentence,word)<<endl;
+            break;
+        case 3:
+            for (int i = 0; i < strlen(sentence); ++i) {
+                sentence[i] = (char)(sentence[i]+(char)8);
+            }
+            cout<<"Encriptacion de la cadena "<<sentence<<endl;
+            //No funciona la pasada por parametros
+            //cout<<encrypt(sentence)<<endl;
+            break;
+        case 4:
+
+            for (int i = 0; i < strlen(sentence); ++i) {
+                sentence[i] = (char)(sentence[i]-(char)8);
+            }
+            cout<<"Desencriptacion de la cadena "<<sentence<<endl;
+            //No funciona la pasado por parametros
+            //cout<< desencripatr(sentence)<<endl;
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            exit(0);
+            break;
+    }
     return 0;
 }
+//Contar el numero de veces que existe una palabra en una cadena
+int WordNumber(char sentence[ ],char word[ ]){
+    int count=0;
+    char *tok;
 
+    tok = strtok(sentence," .,:;");
+    while(tok != NULL){
+        if(stricmp(tok,word)==0){
+            count++;
+        }
+        tok = strtok(NULL," .,:;");
+    }
+    return count;
+}
+
+//Encriptar
+string encrypt(char sentence[]){
+    for (int i = 0; i < strlen(sentence); ++i) {
+        sentence[i] = (char)(sentence[i]+(char)8);
+    }
+    return sentence;
+}
+
+//Desencriptar
+string desencripatr(char sentence[]){
+    for (int i = 0; i < strlen(sentence); ++i) {
+        sentence[i] = (char)(sentence[i]-(char)8);
+    }
+    return sentence;
+}
