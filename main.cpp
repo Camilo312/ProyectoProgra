a1e4c3d21dd4f4ad864eea98a45fb6d87a3c9aaf
jazmin-saleh <jazmin.saleh@uptc.edu.co>
2021-08-14 15:06:27 -0500

    Menu de opciones

--- main.cpp
+++ main.cpp
@@ -2,19 +2,11 @@
 #include <string>
 
 using namespace std;
-
 int main() {
-    std::cout << "Hello KK" << std::endl;
+   cout<<"1.Convertir en nombre propio el contenido de la cadena\n2.Buscar palabra\n3.Encriptar cadena\n4.Desencriptar"
+         "\n5.Llenar cacarter por izquierda o por derecha\n6.Borrar cracteres de una cadena\n7.Interseccion\n8.Diferencia entre dos cadenas"
+         "\n9.Borrar caracteres iziquierda o derecha\n"
+         "10.Validar correo electronico\n11.Salir";
     return 0;
 }
 
-string deleteCharacter(char c, string chain){
-    char temp = NULL;
-    for (char & i : chain) {
-        if(i == c){
-            i = temp;
-            cout<<chain;
-        }
-    }
-    return chain;
-}
